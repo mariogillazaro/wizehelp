929a5fb478b5378e13210060304c376c833a9183
Mario Gil <mario.gil@wizeline.com>
2018-04-18 17:42:34 -0500

    Delete unused directory

--- src/routes/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {Router, Route, BrowserHistory} from 'react-router';
-import App from '../App';
-import Store from '../store/index';

56e63a6fc29e0a622626886d6ef858d2f74e0554
Mario Gil <mario.gil@wizeline.com>
2018-04-18 17:40:29 -0500

    Mock department info

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from './pages/home';
 import Store from './store/index';
+import Departments from './data/departments';
 import linkBuilder from './builders/linkBuilder';
 import DepartmentSection from './builders/sectionBuilder';
 import './App.css';
-const SECTIONS = [
-  {
-    to: '/facilities',
-    text: 'Facilities',
-    component: props => <DepartmentSection {...props}/>
-  }
-];
 
 class App extends React.Component {
   render() {

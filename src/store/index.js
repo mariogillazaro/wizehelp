763721b268446d1d3073445baaf7695e6adc1b85
Mario Gil <mario.gil@wizeline.com>
2018-04-18 20:02:28 -0500

    Apply `redux-logger` middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import {createStore, applyMiddleware} from 'redux';
 import Reducer from '../reducers';
 
-const Store = createStore(Reducer, applyMiddleware(thunk));
+const Store = createStore(Reducer, applyMiddleware(thunk, logger));
 
 export default Store;
